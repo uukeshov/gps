94ab6b23421d51bc9b9f4824983c90e59dcccde8
uukeshov <uukeshov@gmail.com>
2016-10-02 20:17:11 +0600

    first commit

---
+++ app/src/main/java/com/uukeshov/gpslocation/Constants.java
@@ -0,0 +1,9 @@
+package com.uukeshov.gpslocation;
+
+/**
+ * Created by uukeshov on 02.10.2016.
+ */
+
+public final class Constants {
+    public static final Integer PermissionCode = 1;
+}
---
+++ app/src/main/java/com/uukeshov/gpslocation/MapsActivity.java
@@ -0,0 +1,145 @@
+package com.uukeshov.gpslocation;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity implements
+        OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, LocationListener {
+    private boolean mRequestingLocationUpdates = false;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient mGoogleApiClient;
+    private PolylineOptions mPolylineOptions;
+    private LatLng mLatLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        this.buildGoogleApiClient();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        checkPermissionForLocation();
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionForLocation();
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+    }
+
+    public void requestPermissionForLocation() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                },
+                Constants.PermissionCode);
+    }
+
+    private void reloadActivity() {
+        if (Build.VERSION.SDK_INT >= 11) {
+            recreate();
+        } else {
+            Intent intent = getIntent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(intent);
+            overridePendingTransition(0, 0);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        updateCamera();
+        updatePolyline();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (mRequestingLocationUpdates) {
+            LocationRequest mLocationRequest = createLocationRequest();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionForLocation();
+                return;
+            }
+            LocationServices.FusedLocationApi.requestLocationUpdates(
+                    mGoogleApiClient, mLocationRequest, this);
+            initializePolyline();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    private void initializePolyline() {
+        mGoogleMap.clear();
+        mPolylineOptions = new PolylineOptions();
+        mPolylineOptions.color(Color.BLUE).width(10);
+        mGoogleMap.addPolyline(mPolylineOptions);
+    }
+
+    private void updatePolyline() {
+        mPolylineOptions.add(mLatLng);
+        mGoogleMap.clear();
+        mGoogleMap.addPolyline(mPolylineOptions);
+    }
+
+    private void updateCamera() {
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mLatLng, 14));
+    }
+
+    private synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this).addApi(LocationServices.API)
+                .build();
+    }
+
+    private LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+    public void checkPermissionForLocation() {
+        boolean isGranted = false;
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionForLocation();
+            isGranted = true;
+        }
+
+        if (isGranted) {
+            reloadActivity();
+        }
+
+    }
+}
